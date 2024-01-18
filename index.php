e23b88da9724acde94d045b3bbabc66e773bcc16
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2024-01-18 19:28:04 +0100

    ajout hero gauche début

--- index.php
+++ index.php
@@ -6,7 +6,17 @@ require_once __DIR__ . '/function.php';
 ?>
 
 <main>
-    
+    <section class="hero-container">
+        <div class="">
+            <h1 class="font-sulphur-point">Lorem ipsum dolor sit amet consectetur.</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic itaque molestiae laboriosam, sed ex vero quidem officiis.</p>
+            <button class="contact-btn">Nous contacter</button>
+            <button></button><p>Prendre contact</p><a href="tel:0235199191">02 35 19 91 91</a>
+        </div>
+        <div>
+            <img src="" alt="">
+        </div>
+    </section>
 </main>
 
 <?php
