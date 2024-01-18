fac61d15caaaabfac9eebd6dfe82e7ef2ab8cd50
Arsene Salvator <salvator.arsene@free.fr>
2024-01-18 19:22:09 +0100

    Update Nav

--- inc/header.php
+++ inc/header.php
@@ -12,11 +12,11 @@
     <nav class="nav-container">
         <img src="../assets/img/logo.png" alt="logo de Botdefoin" class="header-logo">
         <ul class="ul-container">
-            <li><a href="" class="active-menu">Nos activités</a></li>
-            <li><a href="" class="">Qui nous-sommes</a></li>
-            <li><a href="" class="">Nos agriculteur</a></li>
-            <li><a href="" class="">Actualité</a></li>
-            <li><a href="" class="">Mon compte</a></li>
+            <li><a href="" class="active-menu link-text">Nos activités</a></li>
+            <li><a href="" class="link-text">Qui nous-sommes</a></li>
+            <li><a href="" class="link-text">Nos agriculteur</a></li>
+            <li><a href="" class="link-text">Actualité</a></li>
+            <li><a href="" class="link-text">Mon compte</a></li>
         </ul>
         <button class="contact-btn">Nous contacter</button>
     </nav>
