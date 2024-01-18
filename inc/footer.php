5b9507bbf5b826e6aacef2fabb3d0b673fffd8c9
Arsene Salvator <salvator.arsene@free.fr>
2024-01-18 23:29:50 +0100

    Footer Update 1

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,18 @@
+<section class="footer-section">
+    <img src="../assets/img/starkeu.png" alt="Starkey" srcset="">
+    <img src="../assets/img/phonak.png" alt="Phonak" srcset="">
+    <img src="../assets/img/rexton.png" alt="Rexton" srcset="">
+    <img src="../assets/img/biotone.png" alt="Biotone" srcset="">
+    <img src="../assets/img/signia.png" alt="Signia" srcset="">
+    <img src="../assets/img/resound.png" alt="Resound" srcset="">
+    
+</section>
+<section class="contact-footer-section">
+
+
+</section>
+
+
 
 </body>
 </html>
