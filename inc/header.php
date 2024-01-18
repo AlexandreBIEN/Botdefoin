f529e51a22440cfbdfc49a3e778254f03dfcb0ff
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2024-01-18 18:20:27 +0100

    début style header

--- inc/header.php
+++ inc/header.php
@@ -8,16 +8,16 @@
     <title>Botdefoin - <?= $title ?></title>
 </head>
 <body>
-<header>
-    <nav>
-        <img src="../assets/img/logo.png" alt="logo de Botdefoin">
-        <ul>
-            <li><a href="">Accueil</a></li>
-            <li><a href="">Nos activités</a></li>
-            <li><a href="">Qui nous-sommes</a></li>
-            <li><a href="">Nos agriculteur</a></li>
-            <li><a href="">Actualité</a></li>
+<header class="header-container">
+    <nav class="nav-container">
+        <img src="../assets/img/logo.png" alt="logo de Botdefoin" class="header-logo">
+        <ul class="ul-container">
+            <li><a href="" class="active-menu">Nos activités</a></li>
+            <li><a href="" class="">Qui nous-sommes</a></li>
+            <li><a href="" class="">Nos agriculteur</a></li>
+            <li><a href="" class="">Actualité</a></li>
+            <li><a href="" class="">Mon compte</a></li>
         </ul>
-        <button type="button">Nous contacter</button>
+        <button class="contact-btn">Nous contacter</button>
     </nav>
 </header>
