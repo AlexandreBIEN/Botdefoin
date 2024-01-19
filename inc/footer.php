d057175c2ec1e4e463f1b9bbcba49a061a5c67f8
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2024-01-19 06:57:05 +0100

    add logo and footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,10 +1,24 @@
 <section class="footer-section">
-
+    <img src="/assets/img/Logo-CNRA.png" alt="logo CRNA">
+    <img src="/assets/img/Hectar-logo.png" alt="logo Hectar">
+    <img src="/assets/img/logo-agridees.svg" alt="logo agridées">
+    <img src="/assets/img/foodtech-logo.png" alt="logo foodtech">
+    <img src="/assets/img/dirca-logo.png" alt="logo dirca">
+    <img src="/assets/img/la-champ-logo.jpg" alt="logo la champagne Aube">
     
 </section>
 <section class="contact-footer-section">
-
-
+    <div class="hero-side">
+        <h2 class="footer-contact-title">Contactez-nous pour un éventuel rendez-vous</h2>
+        <div class="footer-yellow-line"></div>
+    </div>
+    <div class="hero-side">
+        <p class="text-contact">Si vous voulez proposer des activités ou seulement pour vous renseigner, n'hésitez pas à nous contacter !</p>
+        <div class="footer-yellow-line"></div>
+        <div class="flex-end">
+            <button class="contact-btn footer-contact-button">Nous contacter maintenant</button>
+        </div>
+    </div>
 </section>
 <footer>
     <div>
