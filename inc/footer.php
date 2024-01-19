40a837fd2e50c71d1a25a8efc058f36c6eb31845
Arsene Salvator <salvator.arsene@free.fr>
2024-01-19 12:22:12 +0100

    Update Top Activite + nav (meilleurs choix) 1

--- inc/footer.php
+++ inc/footer.php
@@ -55,6 +55,6 @@
 <div class="copyright">
     <p>© 2024 - COOP 4.0 - BotdeFoin - La Manu</p>
 </div>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
