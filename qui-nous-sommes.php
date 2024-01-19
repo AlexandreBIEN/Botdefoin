7aade272169461996ab471e06ce69f69deab364b
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2024-01-19 10:38:48 +0100

    début page qui nous sommes

---
+++ qui-nous-sommes.php
@@ -0,0 +1,70 @@
+<?php
+$title = 'Qui nous-sommes';
+require_once __DIR__ . '/inc/header.php';
+require_once __DIR__ . '/function.php';
+
+?>
+
+<main>
+    <section class="hero-container">
+        <div class="hero-side">
+            <h1 class="font-sulphur-point hero-title-home">Botdefoin, on vous aiguille !</h1>
+            <p>Imaginez-vous l'odeur d'un vignoble, le partage autours de produits authentiques, un apéro concert dans les vignes, un explor games chez un maraîcher, la fabrication de  fromage à la ferme, c'est ce qu'on appelle vivre une belle expérience à la ferme. Venez rejoindre la communauté Botdefoin, vivre de belles expériences et créer un lien fidèle avec les agriculteurs.</p>
+            <div class="hero-contact">
+                <button class="contact-btn">Nous contacter</button>
+                <button class="hero-phone"><img src="/assets/img/solar_phone-calling-bold.svg" alt="icon téléphone"></button><div class="hero-contact-container"><p class="hero-text-contact">Prendre contact</p><a href="tel:0235199191">02 35 19 91 91</a></div>
+            </div>
+        </div>
+        <div class="hero-side">
+            <img src="./assets/img/img-moulin.png" alt="" class="hero-img">
+        </div>
+    </section>
+    <section class="hero-container padding-botdefoin">
+        <div class="hero-side">
+            <img src="./assets/img/image_1.png" alt="image famille" class="hero-img">
+        </div>
+        <div class="hero-side">
+            <h1 class="font-sulphur-point hero-title-home">Nous sommes Botdefoin</h1>
+            <p>Nous sommes cinq manager de coopératives agricoles. Nous avons fondé Botdefoin pour consolider et mettre en lumière un marché traditionnel d'expériences à la ferme et permettre à l'agriculteur d'investir dans la différence au-delà de la vente directe : la relation directe et la créativité en adoptant les codes du tourisme, des loisirs et de la culture.</p>
+            <div class="footer-yellow-line"></div>
+        </div>
+    </section>
+    <section class="hero-container padding-botdefoin margin-top-100">
+        <div class="hero-side">
+            <div class="footer-yellow-line margin-bottom-10"></div>
+            <p>Botdefoin œuvre pour la visibilité du monde agricole et des territoires et le partage avec le voyageur, notre engagement est inscrit dans notre raison d'être statutaire comme suit : « Toucher au cœur les agriculteurs et les consommateurs. Œuvrer à connecter deux univers : le monde agricole et le consommateur, pour le bien vivre ensemble, la préservation du bien commun et la création de valeur dans les territoires. »</p>
+        </div>
+        <div class="hero-side">
+            <img src="./assets/img/image_1.png" alt="image famille" class="hero-img">
+        </div>
+    </section>
+    <section class="nos-valeurs-container">
+        <h1 class="font-sulphur-point hero-title-home">Nos valeurs chez Botdefoin</h1>
+        <div class="footer-yellow-line"></div>
+        <p>La modernité et la diversité des expériences pour toucher toutes les générations et en particulier les jeunes.</p>
+        <div class="cards-container">
+            <div class="card-item">
+                <img src="/assets/img/icons8-compte-vérifié-100.png" alt="icon authenticité et modernité" class="card-img">
+                <p class="card-title">Authenticité et modernité</p>
+                <p class="card-content">le partage autour de produits authentiques, qui recrée l'univers de vacances dans la maison des grands-parents, parfois avec un brin de nostalgie.</p>
+                <a href="#" class="card-link">En savoir plus</a>
+            </div>
+            <div class="card-item">
+                <img src="/assets/img/icons8-confiance-90.png" alt="icon authenticité et modernité" class="card-img">
+                <p class="card-title">Confiance et sens</p>
+                <p class="card-content"> la qualité du produit que l'on consomme, le partage autours des métiers, des savoir-faire, des démarches environnementales et sociétales.</p>
+                <a href="#" class="card-link">En savoir plus</a>
+            </div>
+        </div>
+    </section>
+    <section class="section-fondateurs">
+        
+    </section>
+    
+
+</main>
+
+<?php
+require_once __DIR__ . '/inc/footer.php';
+    
+?>
