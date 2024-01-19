399486a47f3c90b97f7964ec69ff66bebeea268b
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2024-01-19 06:03:08 +0100

    del img template + added footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,18 +1,46 @@
 <section class="footer-section">
-    <img src="../assets/img/starkeu.png" alt="Starkey" srcset="">
-    <img src="../assets/img/phonak.png" alt="Phonak" srcset="">
-    <img src="../assets/img/rexton.png" alt="Rexton" srcset="">
-    <img src="../assets/img/biotone.png" alt="Biotone" srcset="">
-    <img src="../assets/img/signia.png" alt="Signia" srcset="">
-    <img src="../assets/img/resound.png" alt="Resound" srcset="">
+
     
 </section>
 <section class="contact-footer-section">
 
 
 </section>
-
-
+<footer>
+    <div>
+        <img src="/assets/img/logo-vide.png" alt="logo Botdefoin">
+        <p class="footer-text-rs">Retrouvez-nous sur : </p>
+        <div>
+            <img src="/assets/img/facebook-logo.png" alt="icon facebook" class="footer-rs">
+            <img src="/assets/img/twitter-logo.png" alt="icon twitter" class="footer-rs">
+            <img src="/assets/img/instagram-logo.png" alt="icon instagram" class="footer-rs">
+        </div>
+    </div>
+    <div>
+        <h3 class="footer-title">Explorez</h3>
+        <div class="footer-line"></div>
+        <ul class="footer-list">
+            <li>Activités</li>
+            <li>Qui sommes-nous ?</li>
+            <li>Nos agriculteurs</li>
+            <li>Actualité</li>
+            <li>Mon compte</li>
+        </ul>
+    </div>
+    <div>
+        <h3 class="footer-title">Divers</h3>
+        <div class="footer-line"></div>
+        <ul class="footer-list">
+            <li>Politique de confidentialité</li>
+            <li>C.G.V.</li>
+            <li>Mention légales</li>
+        </ul>
+    </div>
+    <p></p>
+</footer>
+<div class="copyright">
+    <p>© 2024 - COOP 4.0 - BotdeFoin - La Manu</p>
+</div>
 
 </body>
 </html>
